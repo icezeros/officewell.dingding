c2eeb52c08918f847e8329942c9ac1a0a6ec887f
icezeros <icezeros@163.com>
2017-06-23 18:02:52 +0800

    test

--- config/config.default.js
+++ config/config.default.js
@@ -7,7 +7,7 @@ module.exports = appInfo => {
   config.keys = appInfo.name + '_1498109411281_6745';
   config.middleware = [ 'errorHandler' ];
   config.ddToken = '123456';
-  config.suiteKey = 'suite4xxxxxxxxxxxxxxx      ';
+  config.suiteKey = 'suite4xxxxxxxxxxxxxxx';
   config.aes_key = '5p95wcajc9no5p4v9ailj863u7gnf47zopkoj0vakl6';
   // config
 
