4db0c209cdbb4375b3aff37a57f9511c687642d1
icezeros <icezeros@163.com>
2017-06-27 15:18:05 +0800

    ddtoken

--- config/config.default.js
+++ config/config.default.js
@@ -6,7 +6,7 @@ module.exports = appInfo => {
   // should change to your own
   config.keys = appInfo.name + '_1498109411281_6745';
   config.middleware = [ 'errorHandler' ];
-  config.ddToken = '123456';
+  config.ddToken = 'sdfg7dh74g9DG7d';
   config.suiteKey = 'suitejrn329ssum6e2iki';
   config.aesKey = '5p95wcajc9no5p4v9ailj863u7gnf47zopkoj0vakl6';
   config.suiteSecret = 'CgSxWaHy0faBelBWZrJlETAC2XPcdWk4R4Leun1POmH_YjY7z1rZpuDaLIN_4FeJ';
