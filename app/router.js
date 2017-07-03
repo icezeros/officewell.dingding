55a0275bc0e8aa1033399a745855c3600fd140c1
icezeros <icezeros@163.com>
2017-07-03 19:43:50 +0800

    debug

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = app => {
-  app.get('/', 'home.index');
+  // app.get('/', 'home.index');
   app.resources('dingcb', '/v1/dingcb', 'v1.dingcb');
+  app.resources('home', '/home', 'home');
 };
