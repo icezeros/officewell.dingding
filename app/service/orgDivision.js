0bd2508435e18c90068dfd46b9ff87079fb8d6ce
icezeros <icezeros@163.com>
2017-07-05 13:32:11 +0800

    debug

--- app/service/orgDivision.js
+++ app/service/orgDivision.js
@@ -2,14 +2,14 @@
  * @Author: icezeros
  * @Date: 2017-07-04 16:54:16
  * @Last Modified by: icezeros
- * @Last Modified time: 2017-07-05 13:30:37
+ * @Last Modified time: 2017-07-05 13:32:06
  */
 
 'use strict';
 module.exports = app => {
   class OrgDivision extends app.Service {
     async authScopes(corp) {
-      const { ctx, urlGet } = this;
+      const { ctx } = this;
       const { helper } = ctx;
       // 获取企业token
       const corpToken = await helper.getCorpToken(corp.corpId);
@@ -18,7 +18,7 @@ module.exports = app => {
       let departmentIds = [];
       // 用户表
       let userIds = [];
-      const urlResult = await urlGet(config.authScopesUrl, {
+      const urlResult = await this.urlGet(config.authScopesUrl, {
         access_token: corpToken,
       });
 
@@ -27,22 +27,22 @@ module.exports = app => {
       );
       console.log('==========', departmentIds);
 
-      // for (let i = 0; i < departmentIds.length; i++) {
-      //   const tmpDepartUrl = await urlGet(config.departmentListUrl, {
-      //     access_token: corpToken,
-      //     id: departmentIds[i],
-      //   });
-      //   const tmpDepartIds = tmpDepartUrl.data.department.map(item => item.id);
-      //   departmentIds = departmentIds.concat(tmpDepartIds);
+      for (let i = 0; i < departmentIds.length; i++) {
+        const tmpDepartUrl = await this.urlGet(config.departmentListUrl, {
+          access_token: corpToken,
+          id: departmentIds[i],
+        });
+        const tmpDepartIds = tmpDepartUrl.data.department.map(item => item.id);
+        departmentIds = departmentIds.concat(tmpDepartIds);
 
-      // }
+      }
 
-      // for (let i = 0; i < departmentIds.length; i++) {
-      //   const departmentInfo = await this.getDingDivision(corp.corpId, departmentIds[i]);
-      //   console.log(departmentInfo);
+      for (let i = 0; i < departmentIds.length; i++) {
+        const departmentInfo = await this.getDingDivision(corp.corpId, departmentIds[i]);
+        console.log(departmentInfo);
 
 
-      // }
+      }
 
 
       userIds = userIds.concat(urlResult.data.auth_org_scopes.authed_user);
