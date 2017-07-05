a189a5ba411c98fcc6f4e9eeb6e7b33bdf0e111c
icezeros <icezeros@163.com>
2017-07-05 14:28:20 +0800

    debug

--- app/service/orgDivision.js
+++ app/service/orgDivision.js
@@ -2,7 +2,7 @@
  * @Author: icezeros
  * @Date: 2017-07-04 16:54:16
  * @Last Modified by: icezeros
- * @Last Modified time: 2017-07-05 14:20:07
+ * @Last Modified time: 2017-07-05 14:27:59
  */
 
 "use strict";
@@ -27,26 +27,11 @@ module.exports = app => {
       );
       for (let i = 0; i < departmentIds.length; i++) {
         this.initDepartments(corp, departmentIds[i]);
-        // const tmpDepartUrl = await this.urlGet(config.departmentListUrl, {
-        //   access_token: corpToken,
-        //   id: departmentIds[i]
-        // });
-        // const tmpDepartIds = tmpDepartUrl.data.department.map(item => item.id);
-        // departmentIds = departmentIds.concat(tmpDepartIds);
       }
 
-      // for (let i = 0; i < departmentIds.length; i++) {
-      //   const departmentInfo = await this.getDingDivision(
-      //     corpToken,
-      //     corp.corpId,
-      //     departmentIds[i]
-      //   );
-      //   console.log(this.dataFormat(corp.companyId, departmentInfo));
-      // }
-
-      // userIds = userIds.concat(urlResult.data.auth_org_scopes.authed_user);
+      userIds = userIds.concat(urlResult.data.auth_org_scopes.authed_user);
       // console.log("departmentIds=======", departmentIds);
-      // console.log("userIds=======", userIds);
+      console.log('userIds=======', userIds);
     }
 
     async initDepartments(corp, id) {
@@ -55,13 +40,14 @@ module.exports = app => {
       const corpToken = await helper.getCorpToken(corp.corpId);
       const config = this.app.config;
       // 企业部门id数组
-      let departmentIds = [id];
+      let departmentIds = [ id ];
+      const departments = [];
       // 用户表
       let userIds = [];
       // for (let i = 0; i < departmentIds.length; i++) {
       const tmpDepartUrl = await this.urlGet(config.departmentListUrl, {
         access_token: corpToken,
-        id
+        id,
       });
       const tmpDepartIds = tmpDepartUrl.data.department.map(item => item.id);
       departmentIds = departmentIds.concat(tmpDepartIds);
@@ -74,21 +60,14 @@ module.exports = app => {
           corp.corpId,
           departmentIds[i]
         );
-        console.log(this.dataFormat(corp.companyId, departmentInfo));
+        departments.push(this.dataFormat(corp.companyId, departmentInfo))
+        // console.log(this.dataFormat(corp.companyId, departmentInfo));
       }
+      await this.ctx.model.OrgDivision.create(departments);
 
       // userIds = userIds.concat(urlResult.data.auth_org_scopes.authed_user);
-      console.log("departmentIds=======", departmentIds);
+      console.log('departmentIds=======', departmentIds);
       // console.log("userIds=======", userIds);
-    }
-
-    async create(companyId, data) {
-      const division = {
-        name: data.name,
-        companyId,
-        ding: data
-      };
-      return await this.ctx.model.OrgDivision.create(division);
     }
 
     dataFormat(companyId, data) {
@@ -97,7 +76,7 @@ module.exports = app => {
       return {
         name: data.name,
         companyId,
-        ding: data
+        ding: data,
       };
     }
 
@@ -106,7 +85,7 @@ module.exports = app => {
       // const corpToken = await helper.getCorpToken(corpId);
       const urlResult = await this.urlGet(config.getDepartmentUrl, {
         access_token: corpToken,
-        id
+        id,
       });
       if (urlResult.status === 200 && urlResult.data.errcode === 0) {
         return urlResult.data;
