1e39c3f584cf8161e0b3a550f339a9b843057b00
icezeros <icezeros@163.com>
2017-07-05 21:13:29 +0800

    init

--- app/controller/v1/userAddOrg.js
+++ app/controller/v1/userAddOrg.js
@@ -2,7 +2,7 @@
  * @Author: icezeros.
  * @Date: 2017-07-05 19:14:37.
  * @Last Modified by: icezeros
- * @Last Modified time: 2017-07-05 21:09:30
+ * @Last Modified time: 2017-07-05 21:13:20
  */
 
 'use strict';
@@ -40,7 +40,7 @@ module.exports = app => {
         };
       }
       if (obj.EventType === 'user_add_org') {
-        const addResult = await this.ctx.service.orgUsers.addUser(
+        const addResult = await this.ctx.service.UserCallback.addUser(
           obj.CorpId,
           obj.UserId
         );
