1574c32ae0b6bb4c2a056f19dc54b6925b9598a3
icezeros <icezeros@163.com>
2017-07-05 20:18:25 +0800

    debug

--- app/controller/v1/userAddOrg.js
+++ app/controller/v1/userAddOrg.js
@@ -2,7 +2,7 @@
  * @Author: icezeros.
  * @Date: 2017-07-05 19:14:37.
  * @Last Modified by: icezeros
- * @Last Modified time: 2017-07-05 20:08:54
+ * @Last Modified time: 2017-07-05 20:18:07
  */
 
 'use strict';
@@ -19,10 +19,27 @@ module.exports = app => {
     async create() {
       const body = this.ctx.request.body;
       console.log(body);
+      console.log(this.ctx.query);
+      const data = this.dTalkApiUtil.decrypt(body.encrypt);
+      const obj = JSON.parse(data.message);
+      console.log(obj);
 
-      const msg = this.dTalkApiUtil.decrypt(body.encrypt);
-      console.log(msg);
-
+      // let resut;
+      // if (obj.EventType === 'check_url') {
+      //   result = {
+      //     msg_signature: this.dTalkApiUtil.getSignature(
+      //       query.timestamp,
+      //       query.nonce,
+      //       aesMsg
+      //     ),
+      //     timeStamp: query.timestamp,
+      //     nonce: query.nonce,
+      //     encrypt: aesMsg,
+      //   };
+      // }
+      // // if()
+      // console.log(data);
+      // this.ctx.body = result;
       this.success('No Method');
     }
 
