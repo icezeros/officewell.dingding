444f1dd5f5b3deaaf1062ef1eeccb1be0624729d
icezeros <icezeros@163.com>
2017-07-06 01:29:40 +0800

    debug

--- app/controller/v1/userLeaveOrg.js
+++ app/controller/v1/userLeaveOrg.js
@@ -2,7 +2,7 @@
  * @Author: icezeros.
  * @Date: 2017-07-05 19:14:37.
  * @Last Modified by: icezeros
- * @Last Modified time: 2017-07-05 19:24:16
+ * @Last Modified time: 2017-07-06 01:29:37
  */
 
 'use strict';
@@ -17,7 +17,45 @@ module.exports = app => {
     }
 
     async create() {
-      this.fail('No Method', 405);
+      const body = this.ctx.request.body;
+      const query = this.ctx.query;
+      console.log(body);
+      console.log(this.ctx.query);
+      const data = this.dTalkApiUtil.decrypt(body.encrypt);
+      const obj = JSON.parse(data.message);
+      console.log(obj);
+      const aesMsg = this.dTalkApiUtil.encrypt('success');
+
+      let result;
+      if (obj.EventType === 'check_url') {
+        result = {
+          msg_signature: this.dTalkApiUtil.getSignature(
+            query.timestamp,
+            query.nonce,
+            aesMsg
+          ),
+          timeStamp: query.timestamp,
+          nonce: query.nonce,
+          encrypt: aesMsg,
+        };
+      } else {
+        // if (obj.EventType === 'user_add_org') {
+        console.log('======================');
+        console.log('======================');
+        console.log('======================');
+        console.log('======================');
+        console.log(obj);
+
+        const addResult = await this.ctx.service.userCallback.removeUser(
+          obj.CorpId,
+          obj.UserId,
+          obj.EventType
+        );
+      }
+      // if()
+      console.log(data);
+      this.ctx.body = result;
+      // this.success('No Method');
     }
 
     async update() {
