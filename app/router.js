5c8f6e872fe6c444584c0d3304db8dc942e6c1aa
icezeros <icezeros@163.com>
2017-07-10 13:50:19 +0800

    debug

--- app/router.js
+++ app/router.js
@@ -4,8 +4,6 @@ module.exports = app => {
   // app.get('/', 'home.index');
   app.resources('dingcb', '/v1/dingcb', 'v1.dingcb');
   app.resources('updateCallback', '/v1/update-callback', 'v1.updateCallback');
-  app.resources('userModifyOrg', '/v1/user-modify-org', 'v1.userModifyOrg');
-  app.resources('userLeaveOrg', '/v1/user-leave-org', 'v1.userLeaveOrg');
   app.resources('test', '/v1/test', 'v1.test');
   app.resources('home', '/', 'home');
 };
